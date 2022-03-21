ef01dbef7054e94d2da70a5fee112d316624617b
Tim Tierney <fiachmri@gmail.com>
2022-03-21 23:56:07 +0000

    Update mbedUSB.ino

--- SDM/examples/mbedUSB/mbedUSB.ino
+++ SDM/examples/mbedUSB/mbedUSB.ino
@@ -8,6 +8,7 @@
 PIO pio = pio1;
 uint sm;
 
+
 void core1_worker() {
   uint32_t a = 0;
   int16_t pinput = 0;
@@ -18,15 +19,24 @@ void core1_worker() {
     // helps prevents clicks and pops I think
 
     if (pio_sm_is_tx_fifo_empty(pio, sm)) {
-      pio->txf[sm] = sdm.o4_os32_df2(pinput);
+      pio->txf[sm] = a;
+      pio->txf[sm] = a;
+      pio->txf[sm] = a;
+      pio->txf[sm] = a;
+      pio->txf[sm] = a;
+      pio->txf[sm] = a;
+      pio->txf[sm] = a;
+      pio->txf[sm] = a;
     }
     // if other core sends value
+    //if (false) {
     if (multicore_fifo_rvalid()) {
       uint32_t rec = multicore_fifo_pop_blocking();
 
       //save previous value so we can stuff buffer with it if music paused.
       pinput = (int16_t)(rec);
       a = sdm.o4_os32_df2(pinput);
+      //a = sdm.o1_os32(pinput);
 
       //write to state PIO when its not full
       while (pio_sm_is_tx_fifo_full(pio, sm)) {}
@@ -44,45 +54,49 @@ void setup() {
   gpio_set_dir(23, GPIO_OUT);
   gpio_put(23, 1);
 
-
   // Set the appropriate clock
   set_sys_clock_khz(115200, false);
   uint offset = pio_add_program(pio, &pdm_program);
   sm = pio_claim_unused_sm(pio, true);
-  pdm_program_init(pio, sm, offset, 14, 115200 * 1000 / (48000.0 * 32));
 
+  // PIO configuration
+  pio_sm_config c = pdm_program_get_default_config(offset);
+  sm_config_set_out_pins(&c, 14, 1);
+  sm_config_set_sideset_pins(&c, 15);
+    
+  pio_gpio_init(pio, 14);
+  pio_gpio_init(pio, 15);
+  pio_sm_set_consecutive_pindirs(pio, sm, 14, 2, true);
+  
+  sm_config_set_fifo_join(&c, PIO_FIFO_JOIN_TX);
+  sm_config_set_clkdiv(&c, 115200 * 1000 / (48000.0 * 32));
+  sm_config_set_out_shift(&c, true, true, 32);
+  pio_sm_init(pio, sm, offset, &c);
+  pio_sm_set_enabled(pio, sm, true);
+
+  // USB audio 
   USBAudio audio(true, 48000, 2, 48000, 2);
-
-
-  static uint8_t buf[64];
+  uint8_t buf[96];
   int16_t *in;
-  int16_t outL;
-  int16_t outR;
-  int16_t mono;
-  int16_t pmono;
-
   multicore_launch_core1(core1_worker);
   delay(2000);
+  
 
+  //
   while (1) {
     if (audio.read(buf, sizeof(buf))) {
       in = (int16_t *)buf;
-      for (int i = 0; i < 16; i++) {
+      for (int i = 0; i < 24; i++) {
         // the left value;
-        outL = *in;
+        int16_t outL = *in;
         in++;
         // the right value
-        outR = *in;
+        int16_t outR = *in;
         in++;
         //mono value
-        mono = (outL + outR) / 2;
+        int16_t mono = (outL + outR) / 2;
         multicore_fifo_push_blocking((uint32_t)(mono));
-        // save previous value in case we need to stuff buffer
-        pmono = mono;
       }
-    } else {
-      // basically if you havent read anything stuff buffer with previous value
-      multicore_fifo_push_blocking((uint32_t)(pmono));
     }
   }
 }
